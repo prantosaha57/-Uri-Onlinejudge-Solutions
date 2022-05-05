1efa3028484ffa00cb504da59d988febd7ec2693
Pranto Saha <98037494+prantosaha59@users.noreply.github.com>
2022-05-05 22:50:00 +0600

    Sum of Consecutive Odd Numbers I

---
+++ 1071.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,a,b,sum=0;
+    scanf("%d%d",&a,&b);
+
+    for(i=(b+1);i<a;i++)
+    {
+        if(i%2!=0)
+        {
+            sum = sum+i;
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
+}
